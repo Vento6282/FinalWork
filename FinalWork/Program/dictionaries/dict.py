dc8aff89de6eec995b4f398b2a896d9cb66e2259
AndreevAV <koriolanrus@gmail.com>
2024-08-22 12:51:17 +0300

    Delete folder

--- FinalWork/Program/dictionaries/dict.py
+++
@@ -1,9 +0,0 @@
-animal_classes = {'PackAnimal':'Вьючное животное',
-                'Pet':'Домашнее животное'}
-
-animal_types = {'1':['Рorse', 'Лошадь',animal_classes['PackAnimal']],
-           '2':['Сamel', 'Верблюд',animal_classes['PackAnimal']],
-           '3':['Вonkey', 'Осёл',animal_classes['PackAnimal']],
-           '4':['Сat', 'Кот',animal_classes['Pet']],
-           '5':['Dog', 'Собака',animal_classes['Pet']],
-           '6':['Hamster', 'Хомяк',animal_classes['Pet']]}
